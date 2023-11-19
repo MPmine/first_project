990b3cc8681fa570afae4c3d7e0d826465dbb4ff
MPmine <144527788+MPmine@users.noreply.github.com>
2023-11-19 16:51:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", path_css="/static/css/style.css")
 
 @app.route('/calc')
 def calc():
